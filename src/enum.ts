f92831788f0b1d0a2049ba62b0603a4f10795e23
Haixiang Zhang <34906442+lichever@users.noreply.github.com>
2024-01-07 13:36:40 -0800

    ts enum

--- src/enum.ts
+++ src/enum.ts
@@ -8,7 +8,20 @@ f(Dir.UP);
 f(Dir.DOWN);
 
 console.log(Dir[2] === 'UP');//true
-console.log(Dir.DOWN === 3);//true
+console.log(Dir['3']);
+console.log(Dir['UP']);
+
+console.log(Dir); //enum is compiled to object, numeric enum is compiled to reverse mapping, but string enum is just a normal object
+
+
+enum Dir2 {UP='UP', DOWN='DOWN', LEFT='LEFT', RIGHT='RIGHT' }
+console.log(Dir2.UP === 'UP');//true
+console.log(Dir2);
+
+console.log(Dir2['UP'] === 'UP');//true
+
+
+
 
 
 // literal + union
